8246b3af3c481f817abc2252203854d982bf163d
Daniel Shirabe <scodeiswhite1998@gmail.com>
2024-10-20 08:57:08 +0900

    First Commit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  namespace :admin do
+    resources :categories
+  end
+  devise_for :admins
+  authenticated :admin_user do
+    root to: "admin#index", as: :admin_root 
+  end
+  
+  get "admin" => "admin#index"
+  
+  root "home#index"
+
+  
+end
